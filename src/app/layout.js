26df7212429a85d8e8e8c584d77e44724b9cd895
Zubair Ahmad <mezee2bee@gmail.com>
2026-07-08 23:28:57 +0500

    repo check

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import "./globals.css";
 
 export const metadata = {
   title: "Syscape",
-  description: "Boot into your own digital reality.",
+  description: "Boot into your own digital reality...",
 };
 
 export default function RootLayout({ children }) {
